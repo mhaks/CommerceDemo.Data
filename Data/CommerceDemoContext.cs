5739639d4e0b14320e6f7a39e6de51dc1cc1383c
mhaks <mike.hakanson@gmail.com>
2025-03-12 15:44:24 -0600

    Add project files.

---
+++ Data/CommerceDemoContext.cs
@@ -0,0 +1,31 @@
+﻿using CommerceDemo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceDemo.Data
+{
+    public class CommerceDemoContext : IdentityDbContext<ApplicationUser>
+    {
+        public CommerceDemoContext (DbContextOptions<CommerceDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CommerceDemo.Models.Product> Product { get; set; } = default!;
+        public DbSet<CommerceDemo.Models.ProductCategory> ProductCategory { get; set; } = default!;        
+        public DbSet<CommerceDemo.Models.StateLocation> StateLocation { get; set; } = default!;
+        public DbSet<CommerceDemo.Models.Order> Order { get; set; } = default!;
+        public DbSet<CommerceDemo.Models.OrderProduct> OrderProduct { get; set; } = default!;
+        public DbSet<CommerceDemo.Models.OrderStatus> OrderStatus { get; set; } = default!;
+        public DbSet<CommerceDemo.Models.OrderHistory> OrderHistory { get; set; } = default!;
+
+        
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CommerceDemo.Models.OrderStatus>().ToTable("OrderStatus");
+        }
+    }
+}
